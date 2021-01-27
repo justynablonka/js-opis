0bf91f50313ca111fa752b037304ef7dfe27b979
jblonka <justyna.blonka@gmail.com>
2021-01-27 18:02:06 +0100

    pierwszy commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let paragraph = document.getElementById("opis");
+let button = document.querySelector('button');
+
+button.onclick = function(){
+    paragraph.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ea, praesentium at provident fugiat illum.";
+}
